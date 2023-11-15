57435cf34075eb48f45feff1b87b4e691b49fb5f
Agnieszka Dzwolak <dzwolak.agnieszka@gmail.com>
2023-11-15 12:07:42 +0100

    Add calculator project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-// Add your JavaScript here
+let calculation = '';
+
+function updateCalculation(value) {
+    calculation += value;
+
+    displayCalculation();
+}
+
+function displayCalculation() {
+    document.querySelector('.js-calculation').value = calculation;
+}
+
+function deleteLastCharacter() {
+    calculation = calculation.slice(0, -1);
+    displayCalculation();
+}
